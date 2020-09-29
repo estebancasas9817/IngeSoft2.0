b8e99841fe2ecea53b9b3675bb0e74d91ebf5ac5
Felipe Vanegas <j_vanegas@javeriana.edu.co>
2020-09-29 00:38:52 -0500

    panel de ejrcicios por niveles

---
+++ app/src/main/java/com/company/workpeace/AbdomenPrincipiante.java
@@ -0,0 +1,14 @@
+package com.company.workpeace;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AbdomenPrincipiante extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_abdomen_principiante);
+    }
+}
